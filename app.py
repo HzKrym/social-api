3dc5849e76a3f2e09ec8bac3b3253e61ce9681e5
Artyom Krymov <temichkrimov.hz@ya.ru>
2023-12-17 02:59:41 +0700

    Лень придумывать комментарий

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from sqlalchemy import Integer, String, Column
 from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
 
-class User(Base):
-    __tablename__ = 'user'
-
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String(100), nullable=False)
-    last_name = Column(String(100), nullable=False)
-    username = Column(String(25), unique=True, nullable=False)
-    password = Column(String(100), nullable=False)
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
 db = SQLAlchemy(app, model_class=Base)
+bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError("Password write-only property")
+    
+    @password.setter
+    def set_password(self, password):
+        hash_bytes = bcrypt.generate_password_hash(password)
+        self.password = hash_bytes.decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
+    
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+    
+
 
 with app.app_context():
     db.create_all()
