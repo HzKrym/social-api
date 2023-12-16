07ea3fd74a464a7156488f6823bd30095fa5ec2f
Artyom Krymov <temichkrimov.hz@ya.ru>
2023-12-17 02:29:08 +0700

    Фикс миграции БД

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import Integer, String, Column
 from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    username = Column(String(25), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
