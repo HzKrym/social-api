c4698e1a409e3732f69372918fd91273d6a1c5dc
Artyom Krymov <temichkrimov.hz@ya.ru>
2024-02-05 22:31:39 +0700

    Добавлены сообщения

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, abort, jsonify
+from typing import Self, List
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import and_, or_
+from datetime import datetime
 
 class Base(DeclarativeBase):
     pass
@@ -21,22 +24,105 @@ class User(db.Model):
     username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
-    def check_password(self, password):
+    def check_password(self, password: str):
         return bcrypt.check_password_hash(self.password, password)
     
     @classmethod
-    def find_by_username(cls, username):
+    def find_by_username(cls, username: str) -> Self:
         return cls.query.filter_by(username=username).first()
+    
+class Message(db.Model):
+    __tablename__ = 'message'
+
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(256), nullable=False)
+    from_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    to_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    datetime = db.Column(db.DateTime)
+
+    def __init__(
+            self, 
+            message: str, 
+            from_id: int, 
+            to_id: int
+            ):
+        self.message = message
+        self.from_id = from_id
+        self.to_id = to_id
+        self.datetime = datetime.now()
+
+    def to_dict(self) -> dict:
+        return {
+            'id': self.id,
+            'message': self.message,
+            'from_id': self.from_id,
+            'to_id': self.to_id,
+            'datetime': self.datetime.__str__()
+        }
 
 
 with app.app_context():
     db.create_all()
 
-@app.route("/")
-def main():
-    return "Test"
+def is_not_added_message(list: List[dict], message: Message) -> bool:
+    for message_in_list in list:
+        if message_in_list['from_id'] == message.from_id or message_in_list['to_id'] == message.from_id:
+            return False
+        if message_in_list['from_id'] == message.to_id or message_in_list['to_id'] == message.to_id:
+            return False
+    return True
 
-@app.route("/register", methods=['POST'])
+@app.route('/')
+def main():
+    return 'Test'
+
+@app.route('/send', methods=['POST'])
+def send_message():
+    if not request.json or not 'message' in request.json or not 'from' in request.json or not 'to' in request.json:
+        abort(400)
+    
+    message = request.json.get('message', '')
+    from_id = request.json.get('from', '')
+    to_id = request.json.get('to', '')
+
+    new_message = Message(message, from_id, to_id)
+    db.session.add(new_message)
+    db.session.commit()
+    return ('', 204)
+
+@app.route('/message', methods=['POST'])
+def get_messages():
+    if not request.json or not 'user_id' in request.json:
+        abort(400)
+
+    user_id = request.json.get('user_id', '')
+    messages: List[Message] = Message.query.where(
+        or_(
+            Message.from_id == user_id,
+            Message.to_id == user_id
+        )
+        ).order_by(Message.datetime.desc()).all()
+    last_messages: List[dict] = []
+    for message in messages:
+        if is_not_added_message(last_messages, message):
+            last_messages.append(message.to_dict())
+    return { 'last_messages': last_messages }
+
+@app.route('/user-message', methods=['POST'])
+def get_messages_by_user():
+    if not request.json or not 'user_id' in request.json or not 'friend_id' in request.json:
+        abort(400)
+    
+    user_id = request.json.get('user_id', '')
+    friend_id = request.json.get('friend_id', '')
+    messages: List[Message] = Message.query.order_by(Message.datetime.desc()).all()
+    messages_list: List[dict] = []
+    for message in messages:
+        if (message.from_id == user_id and message.to_id == friend_id) or (message.from_id == friend_id and message.to_id == user_id):
+            messages_list.append(message.to_dict())
+    return { 'messages_list': messages_list }
+
+@app.route('/register', methods=['POST'])
 def register():
     if not request.json or not 'username' in request.json or not 'password' in request.json:
         abort(400)
@@ -45,8 +131,6 @@ def register():
     pswrd = request.json.get('password', '')
     first_name = request.json.get('first_name', '')
     last_name = request.json.get('last_name', '')
-
-
 
     user = User.find_by_username(username)
     if not user == None:
@@ -66,7 +150,7 @@ def register():
     return { 'id': new_user.id }
 
     
-@app.route("/login", methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
     if not request.json or not 'username' in request.json or not 'password' in request.json:
         abort(400)
@@ -80,7 +164,7 @@ def login():
     else:
         abort(401)
     
-@app.route("/user/<username>", methods=['GET'])
+@app.route('/user/<username>', methods=['GET'])
 def user(username):
     user = User.find_by_username(username)
     return {
